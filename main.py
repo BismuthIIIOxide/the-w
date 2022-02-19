1dc05d62b0cf7feeabb18e99950c286003ddc8a1
BismuthIIIOxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2022-02-18 23:28:48 -0500

    Remove cogs, didn't seem to work

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ import discord
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='g!')
-T = os.environ['TOKEN']
 
-for f in os.listdir("./cogs"):
-    if f.endswith(".py"):
-        client.load_extension("cogs." + f[:-3])
+T = os.environ['TOKEN']
 
 @client.event
 async def on_ready():
-    print(f'Logged on as {client.user}\n\n\n')
+    print(f"Logged into {client.user} : {client.id}\n\n\n")
 
 
+@client.command()
+async def fetch(ctx):
+    # 851855270685835264
+    # 702972566419144875
+    channel = client.get_channel(851855270685835264)
+    messages = await channel.history(limit=10).flatten()
+
+    for msg in messages:
+        if len(msg.embeds)>0:
+            embed = msg.embeds[0]
+            await ctx.channel.send(f"From {msg.guild}\n{embed.title} : {embed.description}")
 
 client.run(T)
