95c14cf2988f0b45cc4250d009d5d6eb0d841fb9
BismuthIIIOxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2022-02-18 23:19:22 -0500

    Cog testing

--- main.py
+++ main.py
@@ -5,26 +5,17 @@ from discord.ext import commands
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 There are a number of utility commands being showcased here.'''
-
 client = commands.Bot(command_prefix='g!', description=description)
-
 T = os.environ['TOKEN']
+
+for f in os.listdir("./cogs"):
+    if f.endswith(".py"):
+        client.load_extension("cogs." + f[:-3])
 
 @client.event
 async def on_ready():
     print('Logged on as', client.user)
 
 
-@client.command()
-async def fetch(ctx):
-    # 851855270685835264
-    # 702972566419144875
-    channel = client.get_channel(851855270685835264)
-    messages = await channel.history(limit=10).flatten()
-
-    for msg in messages:
-        if len(msg.embeds)>0:
-            embed = msg.embeds[0]
-            await ctx.channel.send(f"From {msg.guild}\n{embed.title} : {embed.description}")
 
 client.run(T)
