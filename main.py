162fad5b36dd8a26e39e30fd5b9bfe5c16368728
Bismuth(III) Oxide <60783577+BismuthIIIOxide@users.noreply.github.com>
2022-02-19 21:20:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ async def fetch(ctx):
     # 851855270685835264
     # 702972566419144875
     channel = client.get_channel(851855270685835264)
-    messages = await channel.history(limit=10).flatten()
-
-    for msg in messages:
-        if len(msg.embeds)>0:
-            embed = msg.embeds[0]
-            await ctx.channel.send(f"From {msg.guild}\n{embed.title} : {embed.description}")
+    messages = await channel.history(limit=100).flatten()
+    if (ctx.channel.id == 944452868218437632 or ctx.channel.id == 427546996178419714):    
+        for msg in messages:
+            if (len(msg.embeds)>0 and msg.author.name=="Auto Upload Bot"):
+                embed = msg.embeds[0]
+                await ctx.channel.send(f"From {msg.guild}\n{embed.title}\n{embed.description}\n------------------")
 
 client.run(T)
